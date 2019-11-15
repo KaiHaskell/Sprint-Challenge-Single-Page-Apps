6b82677e70a493228b6ceb3fa9b5c84bbf05bd25
kai-haskell <me@kaihaskell.com>
2019-11-15 12:51:43 -0500

    got characters to render

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
 import React from "react";
+import styles from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card = styles.div`
+width: 10rem;
+
+`;
+
+export default function CharacterCard({ name, species, status, image }) {
+  return (
+    <Card>
+      <img src={image} className="card-img-top" alt="profile" />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">
+          {species}
+          {status}
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </Card>
+  );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,13 +1,24 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
+        <Link to="/characters">
+          <button type="button" className="btn btn-info">
+            Characters
+          </button>
+        </Link>
+        <Link to="/search">
+          <button type="button" className="btn btn-info">
+            Search
+          </button>
+        </Link>
+
         <img
           className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          src="https://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2017/10/rick-and-morty-season-4.png?itok=ttETnHjk"
           alt="rick"
         />
       </header>
